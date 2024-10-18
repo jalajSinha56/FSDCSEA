2acd8884dcc56f96d6c7611265ad10ddf944cf43
jalajsinha56 <jalajsinha140903@gmail.com>
2024-10-18 14:10:02 +0530

    JS work

---
+++ JSWork/index.js
@@ -0,0 +1,63 @@
+// console.log("Hello World!")
+// var a = 12
+// console.log("a="+a)
+// if(a>10){
+//     var a=30;
+//     console.log("Hi",a)
+// }
+// console.log("Hello ",a)
+
+// let c = 12
+// let b = "12"
+// let sim = Symbol("122")
+// let today = Date()
+// console.log(typeof today)
+// console.log(today) 
+
+// let a = 12
+// let b = '12'
+// if(a==b){
+//     console.log("a==b is true")
+// }
+// else{
+//     console.log("a==b is false")
+// }
+// if(a===b){
+//     console.log("a===b is true")
+// }
+// else{
+//     console.log("a===b is false")
+// }
+
+// let d = function (str = "Hello"){
+//     console.log("Hi function calling "+str)
+// }
+// d("JS and CSS");
+
+// function sum(a,b,c=50){
+//     return a+b+c
+// }
+// let total = sum(3,4,100)
+// console.log("Total = "+ total)
+
+
+// function selectlanguage(lang){
+//     let choice;
+//     if(lang == 'C'){
+//         function cCompiler(){
+//             return "c compiler selected";
+//         }
+//         choice = cCompiler();
+//     }
+//     else if(lang == 'Java'){
+//         function javaCompiler(){
+//             return "java compiler selected";
+//         }
+//         choice = javaCompiler();
+//     }
+//     return choice;
+// }
+// console.log(selectlanguage('Java'))
+
+console.log("Hello JS")
+console.log(document);
